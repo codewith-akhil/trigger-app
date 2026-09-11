505e6522289406c210b9a1c8e0e83a6589bd355d
Z User <z@container>
2026-09-11 23:16:36 +0000

    chore(release): versionCode 12 / versionName 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.trigger.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 11
-    versionName = "1.0.11"
+    versionCode = 12
+    versionName = "1.0.12"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
